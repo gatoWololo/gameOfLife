ddc66adb554ebf629c498f3477de815f44e968f6
omar <gatoWololo@gmail.com>
2014-08-27 20:17:30 -0700

    Added Rules to the game and next generation array iterations can now be made. Will work on adding fuctionality for multiple generation interations. Probably State Monad?

--- Grid.hs
+++ Grid.hs
@@ -1,37 +1,37 @@
-module Grid (lifeGrid,gridSize,printArray,findNumber,Board) where
+{- | General functions for array operations. -}
+module Grid (grid2D,printArray,findNumber,Array2D,getElem,gridSize) where
 import Data.Array.Repa
 
-lifeGrid = (Z :. gridSize :. gridSize :: DIM2)
-type Board = Array U DIM2 Int
+type Array2D = Array U DIM2 Int
 type Coordinate = (Int,Int)
 
-gridSize :: Int
-gridSize = 30
+gridSize = 10
 
-{- | Give a grid a number will find the sum of the neighbors. As needed by the rules of Game of Life. -}
-findNumber:: Board -> Coordinate -> Int
+grid2D = Z :. gridSize :. gridSize :: DIM2
+
+{- | Give a grid a number will find the sum of adjacent cells. -}
+findNumber:: Array2D -> Coordinate -> Int
 findNumber grid x = (getSum grid . filterNeighbors . getNeighbors) x
 
-{- | Return the element at the (x,y) spot given an array -}
-getElem :: Board -> Coordinate -> Int
+{- | Return the element at the (x,y) spot given an array. -}
+getElem :: Array2D -> Coordinate -> Int
 getElem grid (n,m) = grid ! (Z :. n :. m)
 
-printArray :: Board -> String
+printArray :: Array2D -> String
 printArray grid = unlines [unwords [show (getElem grid (x,y)) | x <- [0..n]] | y <- [0..n]]
-    where n = gridSize - 1
+    where n = gridSize -1
 
 {- | Returns a list of the neighbors to that index. Note: does not care if out of bounds in array -}
 getNeighbors:: Coordinate -> [Coordinate]
 getNeighbors (n,m) = [(n+x,m+y)|x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0]
 
-{- | Since some neighbors may be out of bounds we will weed these guys out for simplicity.
-   In the future we may wrap around the array, to feel cool we will call it a torus B) -}
+{- | Since some adjacent cells may be out of bounds we will weed these guys out for simplicity.
+   In the future we may wrap around the array. We will call it a torus at that point B) -}
 filterNeighbors:: [Coordinate] -> [Coordinate]
 filterNeighbors xs = filter (\(x,y) -> check x && check y) xs
     where n = gridSize
-          check x = x <= n && x >= 0
+          check x = x < n && x >= 0
 
 {- | Given a list of tuples and a matrix will return the sum of adding the number at those spots -}
-getSum:: Board ->[Coordinate] -> Int
+getSum:: Array2D -> [Coordinate] -> Int
 getSum grid list = foldr (\x -> ((getElem grid x) +)) 0 list
-
--- Main.hs
+++ Main.hs
@@ -5,23 +5,12 @@ module Main where
 
 import System.Environment(getArgs)
 import System.Random
-import Data.Array.Repa
-import Grid
+import Grid(printArray,gridSize)
+import Life(getInitGrid,nextLife)
 
 main :: IO()
 main = do
-  randStream <-  makeRandList
-  let randList = take  (gridSize*gridSize) randStream
-      --We have created our grid of randomly assinged life.
-      grid = fromListUnboxed lifeGrid randList
+  grid <- getInitGrid gridSize
+  nextGrid <- nextLife grid
   putStrLn $ printArray grid
-  putStrLn . show $ findNumber grid (0,0)
-
-
-
-{- | Creates Lazy list of random 1's and 0's. -}
-makeRandList:: IO [Int]
-makeRandList = do
-  g <- newStdGen
-  return $ randomRs (0,1) g
-
+  putStrLn $ printArray nextGrid
